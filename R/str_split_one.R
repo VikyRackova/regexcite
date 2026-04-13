cfd7ce8832324e9a7214d803e702cd5058939e92
Viky Rackova <viki.rackova@gmail.com>
2026-04-13 12:50:31 +0200

    read me working

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -10,6 +10,7 @@
 #' x <- "alpha, beta, gamma, delta"
 #' str_split_one(x,pattern = ",")
 #' str_split_one(x,pattern = ",", n = 2)
+
 str_split_one<- function(string,pattern, n= Inf){
   stopifnot(is.character(string), length(string)<=1)
   if (length(string)==1){
